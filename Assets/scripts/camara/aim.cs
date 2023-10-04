6c77b363589156d013b55fd65b93dcc75daaf494
Skyemoure <53685333+Skyemoure@users.noreply.github.com>
2023-10-05 00:51:09 +0200

    zoom

---
+++ Assets/scripts/camara/aim.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aim : MonoBehaviour
+{
+ 
+    
+    public GameObject mainCamera;
+    public GameObject aimCamera;
+   
+    public GameObject follow2;
+    public GameObject follow1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(1) && !aimCamera.activeInHierarchy)
+        {
+            mainCamera.SetActive(false);
+            aimCamera.SetActive(true);
+
+            RaycastHit hit;
+            
+            if (Physics.Raycast(follow2.transform.position, follow2.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                
+                Debug.Log("colisiono");
+            }
+
+
+           
+        }
+        else if (!Input.GetMouseButton(1) && !mainCamera.activeInHierarchy)
+        {
+            mainCamera.SetActive(true);
+            aimCamera.SetActive(false);
+            
+
+        }
+
+    }
+
+   
+    
+}
+
