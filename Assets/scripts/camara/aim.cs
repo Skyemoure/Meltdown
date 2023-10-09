e2d3d7ea659f60606205a851e55ecb4b35c5045f
Guillermo Zolessi <146029340+guiislost@users.noreply.github.com>
2023-10-09 18:49:06 -0300

    Versión 0.2 mira

--- Assets/scripts/camara/aim.cs
+++ Assets/scripts/camara/aim.cs
@@ -12,6 +12,10 @@ public class aim : MonoBehaviour
     public GameObject follow2;
     public GameObject follow1;
     public GameObject botella;
+    public GameObject mira;
+
+    Vector3 dir;
+    GameObject i;
     public Transform mano;
     // Start is called before the first frame update
     void Start()
@@ -22,22 +26,23 @@ public class aim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButton(1))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                i = Instantiate(botella, mano.transform.position, Quaternion.identity);
+                dir = mira.transform.position - mano.transform.position;
+                i.GetComponent<Rigidbody>().AddForce(dir * 100f, ForceMode.Force);
+            }
+        }
+
+
+
         if (Input.GetMouseButton(1) && !aimCamera.activeInHierarchy)
         {
             mainCamera.SetActive(false);
-            aimCamera.SetActive(true);
-
-           
-            RaycastHit hit;
-            
-            if (Physics.Raycast(follow2.transform.position, follow2.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
-            {
-
-                Instantiate(botella, mano.transform.position, Quaternion.identity);
-
-
-            }
+            aimCamera.SetActive(true);    
           
         }
         else if (!Input.GetMouseButton(1) && !mainCamera.activeInHierarchy)
