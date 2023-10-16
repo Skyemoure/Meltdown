1b849c0807525595f5604b0059a2c2192b239cae
TheLegendaryDogeborn <71360772+ElConde94@users.noreply.github.com>
2023-10-16 18:55:10 -0300

    Es mi rama y tiene un frijol
    Nacio un frijol wtf? no es una rama real

---
+++ Assets/frijol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frijol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("No mames wey un frijol cuadrado");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
