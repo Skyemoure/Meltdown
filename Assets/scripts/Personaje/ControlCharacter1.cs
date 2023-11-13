7a8802cc16c0c34c87004ce98f687dca2f0a4f83
Guillermo Zolessi <146029340+guiislost@users.noreply.github.com>
2023-11-13 18:41:22 -0300

    Movimiento 1.2
    Arreglo del movimiento, y agrego comentarios al código.

--- Assets/scripts/Personaje/ControlCharacter1.cs
+++ Assets/scripts/Personaje/ControlCharacter1.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class ControlCharacter1 : MonoBehaviour
 {
+
     static public int botellas;
     private CharacterController controller;
     private Vector3 playerVelocity;
@@ -23,62 +24,48 @@ public class ControlCharacter1 : MonoBehaviour
     GameObject i;
     public Transform mano;
 
+
+    //Variables de velocidad, gravedad
     [SerializeField]
     private float playerSpeed = 2.0f;
-    //private float jumpHeight = 1.0f;
+    private float jumpHeight = 0f;
     [SerializeField]
     private float gravityValue = -9.81f;
     [SerializeField]
     private float rotSpeed = 0.8f;
 
+    //Número que marca el ritmo de movimiento, se usa para limitar la velocidad del player por el mismo número
+    [SerializeField]
+    private float timeSpeed = 0.08f;
+
     private void Start()
     {
-
+        //inicializo variables
         botellas = 5;
+        //busco controller y el input de player creado
         controller = GetComponent<CharacterController>();
         input = GetComponent<PlayerInput>();
         moveAction = input.actions["Move"];
-        lookAction = input.actions["Look"];
+        //actuan diferentemente las mismas teclas segun las acciones activadas
+        lookAction = input.actions["Look"]; 
+        //camara
         camTransform = Camera.main.transform;
 
+        //para que el cursor no salga de la pantalla
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-
-        Vector2 input = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
-        move = move.x * camTransform.right.normalized + move.z * camTransform.forward.normalized;
-        move.y = 0;
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //Rotar personaje hacia la camara
-        float targetAngle = camTransform.eulerAngles.y;
-        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotSpeed * Time.deltaTime);
-
-        /*
-        // Changes the height position of the player..
-        if (jumpAction.triggered && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }*/
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+       
 
         //botellas
         if (Input.GetMouseButton(1))
         {
             if (Input.GetMouseButtonDown(0) && botellas > 0)
             {
+                //Creamos botellas al disparar
+                //Si no golpea a nada sigue infinitamente (hasta que se le vá la vida útil y se autodestruye)
                 RaycastHit hit;
                 i = Instantiate(botella, transform.position, Quaternion.identity);
                 arrojarBotella bulletController = i.GetComponent<arrojarBotella>();
@@ -102,8 +89,53 @@ public class ControlCharacter1 : MonoBehaviour
         }
 
     }
+
+    private void FixedUpdate()
+    {
+        //chequeamos si hay piso
+        groundedPlayer = controller.isGrounded;
+
+        if(groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        //Leemos el input para mover al jugador según su rotación
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = move.x * camTransform.right.normalized + move.z * camTransform.forward.normalized;
+        move.y = 0;
+        controller.Move(move * timeSpeed * playerSpeed);
+
+        //Rotar personaje hacia la camara
+        float targetAngle = camTransform.eulerAngles.y;
+        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotSpeed * timeSpeed);
+
+
+
+
+
+       //Salto teórico si hubiera, ahora mismo no hace nada
+       /* if (groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }*/
+
+        //gravedad y límite
+        playerVelocity.y += gravityValue * timeSpeed;
+        if (playerVelocity.y < -15f)
+        {
+            playerVelocity.y = -15f;
+        }
+        
+
+        controller.Move(playerVelocity * timeSpeed);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        //Recargar botellas al agarrar más
         if (other.tag == "pickup")
         {
             botellas += 5;
