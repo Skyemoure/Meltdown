3ac44c9895988ed44dc8947e0a3496d7a9d207ef
Skyemoure <53685333+Skyemoure@users.noreply.github.com>
2023-12-05 22:26:17 +0100

    animaciones

--- Assets/scripts/Enemigos/TimerEnemigos.cs
+++ Assets/scripts/Enemigos/TimerEnemigos.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
+public class TimerEnemigos : MonoBehaviour
 {
     public Text tiempo;
     public float puntos;
